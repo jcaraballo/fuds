f4cf223fbb94ab334180f8d71b51feeafbbde198
jcaraballo <git@caraballo.eu>
2014-07-17 00:49:43 +0100

    Test HTTPS
    (disabling certificate checking while it's not supported by http-naive)

--- src/test/scala/acceptance/FudsHttpsSpec.scala
+++ src/test/scala/acceptance/FudsHttpsSpec.scala
@@ -2,33 +2,24 @@ package acceptance
 
 import org.scalatest.{BeforeAndAfterEach, Spec}
 import fuds.Fuds
-import io.shaka.http.Http.http
 import io.shaka.http.Request.{PUT,GET}
-import io.shaka.http.Response
-import io.shaka.http.Status.{FORBIDDEN, NOT_FOUND, OK, BAD_REQUEST}
-import fuds.restriction.IsCsv
-import java.io.ByteArrayInputStream
-import java.nio.charset.StandardCharsets
+import io.shaka.http.{TrustAllSslCertificates, Response}
+import io.shaka.http.Status.OK
+import io.shaka.http.Http.http
 
 class FudsHttpsSpec extends Spec with BeforeAndAfterEach {
-
   object `Server must` {
     def `enable users to upload files via HTTPS`() {
-      pending // naive-http doesn't support https (yet)
-
       val response: Response = http(PUT(base + "/fear.csv").entity("foo,bar\n1,2\n"))
       assert((response.status, body(response)) === (OK, Some("/fear.csv")))
     }
 
     def `enable users to retrieve files previously uploaded via HTTPS`() {
-      pending // naive-http doesn't support https (yet)
-
       assert(okBody(http(PUT(base + "/fear.csv").entity("foo,bar\n1,2\n"))) === "/fear.csv")
       
       val response = http(GET(base + "/fear.csv"))
       assert((response.status, body(response)) === (OK, Some("foo,bar\n1,2")))
     }
-
   }
 
   private def body(response: Response): Option[String] = response.entity.map(_.toString().trim)
@@ -39,6 +30,7 @@ class FudsHttpsSpec extends Spec with BeforeAndAfterEach {
 
   override def beforeEach(){
     fuds = Fuds.startHttps()
+    TrustAllSslCertificates
   }
 
   override def afterEach(){
