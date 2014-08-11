8e28a8257300e311c506284f459cc4e394e31afb
jcaraballo <git@caraballo.eu>
2014-08-11 11:20:28 +0100

    Run smoke tests in CI

--- smoke.bash
+++ smoke.bash
@@ -11,15 +11,15 @@ fuds_pid=$!
 mkdir ${temp}/storage && echo foo >${temp}/storage/foo || moan "Failed to create foo"
 
 times_to_try=10
-until curl -s3 --cacert src/test/resources/certs/fuds-local.cacert --fail https://localhost:8888/foo >${temp}/downloaded_foo || [ $times_to_try -eq 0 ]; do
+until curl -s3 --cacert src/test/resources/certs/fuds-local.cacert --fail https://localhost:8888/foo >${temp}/downloaded_foo || [ ${times_to_try} -eq 0 ]; do
   echo Retrying foo retrieval up to $(( times_to_try-- )) times >&2
   sleep 1
 done
 
-if [ $times_to_try -eq 0 ] ; then
+if [ ${times_to_try} -eq 0 ] ; then
   moan "Never managed to download foo"
 fi
 
 cmp ${temp}/storage/foo ${temp}/downloaded_foo || moan 'Comparison between original and downloaded foo failed'
 
-kill $fuds_pid
+kill ${fuds_pid}
