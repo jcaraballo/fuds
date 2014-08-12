129f7180d25f6d646996347f22ce97f8997a7b1e
jcaraballo <git@caraballo.eu>
2014-08-12 11:21:24 +0100

    publish to hallon skipping certificate check
    (eat your own dog food part 1)

---
+++ publish.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$( dirname "$0" )"
+source function_moan.bash
+
+artefact_path=$( echo target/scala-2.11/fuds-*.jar )
+artefact_filename=$( basename $artefact_path )
+version=$( echo $artefact_filename | sed 's|fuds-||' | sed 's|\-.*.jar||' )
+
+if [[ "${FUDS_PUBLISHER_PASSWORD}" == "" ]] ; then
+  credentials=publisher
+else
+  credentials=publisher:${FUDS_PUBLISHER_PASSWORD}
+fi
+
+target_url=https://37.187.23.147:9010/fuds/${version}/${artefact_filename}
+echo "Publishing fuds version ${version}: ${artefact_path} -> ${target_url}" >&2
+curl -s --fail -k3T ${artefact_path} --user $credentials $target_url >/dev/null || moan "Failed to publish ${artefact_path} to ${target_url}"
+
+echo Success >&2
