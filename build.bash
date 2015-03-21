d2a77a5f6d68cfd568faced4c70af0fb33349de7
jcaraballo <git@caraballo.eu>
2015-03-21 01:40:11 +0000

    Add quotes around # character
    so it is not considered a Bash comment

--- build.bash
+++ build.bash
@@ -6,9 +6,9 @@ cd "$( dirname "$0" )"
 
 if [[ "$TRAVIS_PULL_REQUEST" == 'false' && "$TRAVIS_BRANCH" == 'master' ]] ; then
 
-  echo ######## DIST ######## && ./dist.bash && \
-    echo ######## SMOKE ######## && ./smoke.bash && \
-    echo ######## PUBLISH ######## && ./publish.bash
+  echo '######## DIST ########' && ./dist.bash && \
+    echo '######## SMOKE ########' && ./smoke.bash && \
+    echo '######## PUBLISH ########' && ./publish.bash
 else
   ./dist.bash && ./smoke.bash
 fi
